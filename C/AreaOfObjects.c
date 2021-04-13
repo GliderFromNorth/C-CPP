4c45cd4345bf4fd93ee4dc3179343246dbdfe60b
Saurav Jha <jhasaurav@outlook.com>
2021-04-13 17:12:13 +0530

    s

---
+++ C/AreaOfObjects.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+Code By: LinkedIn.com/in/GliderFromNorth
+Topic  :Area of different objects. 
+
+*/
+int main(){
+    int l, b;
+    printf( "Enter the length of rectable:");scanf("%d",&l);
+    printf( "Enter the breadth of rectable:");scanf("%d",&b);
+    printf( "Area of rectable is: %d", l*b);
+    return 0;
+}
